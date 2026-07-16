df692850591ba773981b2835d2b4f0e62807a2fd
xCuzSkillz <pvpplayer403020@gmail.com>
2026-07-16 11:28:16 +0300

    User Model

--- models/User.js
+++ models/User.js
@@ -11,5 +11,17 @@ const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         trim: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean
     }
-})
+}, {timestamps: true})
+
+const User = mongoose.model("User", UserSchema)
+
+module.exports = User;
